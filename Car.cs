5cfb02bcffb623be371112233dc4db6909d28615
Benia Morgan <bmorganware@gmail.com>
2024-11-21 11:37:37 -0500

    constructors added to base class

--- Car.cs
+++ Car.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 namespace CarsAndUsedCars
 {
-	public class Car
+	internal class Car
 	{
+		public string Make { get; set; }
+										
+		public string Model { get; set; }
+
+		public int Year { get; set; }
+
+		public decimal Price { get; set; }
+
 		public Car()
 		{
+			Make = "";
+			Model = "";
+			Year = 0;
+			Price = 0;
+		}
+
+		public Car(string make, string model, int year, decimal price)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			Price = price;
 		}
 	}
 }
